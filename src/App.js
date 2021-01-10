5a2b57fef021668ab66bac6a8b144af4431b3fd9
axcamz <61030878+axcamz@users.noreply.github.com>
2021-01-10 12:27:10 +0700

    Setup tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/app.css'
+import { Container } from './components/atoms';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-blue-800">
+      <Container>
+        <h1 className="text-white">Hello World</h1>
+      </Container>
     </div>
   );
 }
---
+++ src/components/atoms/Container.js
@@ -0,0 +1,9 @@
+const Container = ({children}) => {
+    return (
+        <div className="lg:container lg:mx-auto lg:px-0 px-3 ">
+            {children}
+        </div>
+    )
+}
+
+export default Container
